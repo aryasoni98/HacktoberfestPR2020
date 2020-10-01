e42a7f2241487391fbb5170318eff39a5c4fa3e8
Ashray <ashrayk11@gmail.com>
2020-10-01 13:26:23 +0530

    added script

---
+++ scripts/HelloWorld_Ashray11.cpp
@@ -0,0 +1,10 @@
+// Author : Ashray Kothari
+// Github : https://github.com/Ashray11
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    return 0;
+}
