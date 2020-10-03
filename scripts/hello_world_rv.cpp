5f4af1b97beb61b34f3c3f87cefb488084fcdb85
= <riyavaryani01@gmail.com>
2020-10-03 19:56:25 +0530

    Hello World in c++

---
+++ scripts/hello_world_rv.cpp
@@ -0,0 +1,11 @@
+//language:- C++14
+//Program to output hello world
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      cout<<"HELLO WORLD!";
+      cout<<endl;
+      return 0;
+}
