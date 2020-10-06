3d7173f0220f855dd29b6bfe7aa2aee6945c2a37
Adil-S786 <71209995+Adil-S786@users.noreply.github.com>
2020-10-07 00:20:40 +0530

    Add files via upload

---
+++ scripts/selection_sort.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+vod xchng(int a,int b)
+int main()
+{
+int a[10],n,i;
+printf("enter the width of array");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+xchng(a,n);
+for(i=0;i<n;i++)
+printf("%d\n",a[i]);	
+}
+void xchng(int a[10],)
